71702a6fee1eb5ca3589100be6cd773a8392989f
acorn1066 <acorn1066@gmail.com>
2026-06-05 12:46:17 +0900

    으음 일단 개인 브랜치에 올리기

---
+++ src/main/java/kh/ddeonabom/schedule/service/ScheduleService.java
@@ -0,0 +1,11 @@
+package kh.ddeonabom.schedule.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleService {
+	
+}
